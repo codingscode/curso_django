a6e489df723476974ebd48b2e741d1bc55203249
codingscode <heronnestg@hotmail.com>
2021-12-03 21:10:22 -0300

    parte 12

--- proj1/django1/urls.py
+++ proj1/django1/urls.py
@@ -14,15 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
-from nucleo.views import index, contato
+
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', index),
-    path('contato', contato),
+    path('', include('nucleo.urls')),
 ]
 
 
---
+++ proj1/nucleo/urls.py
@@ -0,0 +1,18 @@
+
+from django.urls import path
+
+from .views import index, contato
+
+
+urlpatterns = [
+    path('', index),
+    path('contato', contato)
+]
+
+
+
+
+
+
+
+
