fba2366f936a9a7dbb99b66307ae53387f81875d
codingscode <heronnestg@hotmail.com>
2021-12-04 12:21:02 -0300

    parte 14

--- proj1/nucleo/views.py
+++ proj1/nucleo/views.py
@@ -2,7 +2,10 @@ from django.shortcuts import render
 
 
 def index(request):
-    return render(request, 'index.html')
+    context = {
+        'curso': 'Prog Django', 'outro': 'javascript'
+    }
+    return render(request, 'index.html', context)
 
 
 def contato(request):
