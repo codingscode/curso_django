77f6d574ce85f5c8383508da7871a21f1cf09a07
codingscode <heronnestg@hotmail.com>
2021-12-07 12:31:12 -0300

    parte 17

--- proj1/nucleo/models.py
+++ proj1/nucleo/models.py
@@ -6,8 +6,10 @@ class Produto(models.Model):
     estoque = models.IntegerField('Quantidade em estoque')
 
 
-
-
+class Cliente(models.Model):
+    nome = models.CharField('Nome', max_length=100)
+    sobrenome = models.CharField('Sobrenome', max_length=100)
+    email = models.EmailField('E-mail', max_length=100)
 
 
 
