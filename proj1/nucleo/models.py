a8e1f1e09e6a71fbfc7201b8fbae0dd5aa61f6ca
codingscode <heronnestg@hotmail.com>
2021-12-10 11:41:34 -0300

    parte 23

--- proj1/nucleo/models.py
+++ proj1/nucleo/models.py
@@ -6,7 +6,7 @@ class Produto(models.Model):
     estoque = models.IntegerField('Quantidade em estoque')
 
     def __str__(self):
-        return f'{self.nome}: {self.estoque}'
+        return self.nome
 
 
 class Cliente(models.Model):
@@ -14,8 +14,9 @@ class Cliente(models.Model):
     sobrenome = models.CharField('Sobrenome', max_length=100)
     email = models.EmailField('E-mail', max_length=100)
 
-
-
+    def __str__(self):
+        return self.nome
+    
 
 
 
