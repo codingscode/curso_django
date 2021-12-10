9262615b7539a1ad7fe71e205b4107cb1a6000e6
codingscode <heronnestg@hotmail.com>
2021-12-09 22:13:56 -0300

    parte 19

--- proj1/nucleo/admin.py
+++ proj1/nucleo/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
-# Register your models here.
+
+from .models import Produto, Cliente
+
+
+
+admin.site.register(Produto)
+admin.site.register(Cliente)
+
+
+
+
+
+
+
+
+
+
+
