6c9db174cda76b688f82e3d2e5bdb40a9fd7c556
codingscode <heronnestg@hotmail.com>
2021-12-10 11:49:11 -0300

    parte 24

--- proj1/nucleo/admin.py
+++ proj1/nucleo/admin.py
@@ -4,14 +4,12 @@ from django.contrib import admin
 from .models import Produto, Cliente
 
 
+class ProdutoAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'preco', 'estoque')
 
-admin.site.register(Produto)
+
+admin.site.register(Produto, ProdutoAdmin)
 admin.site.register(Cliente)
-
-
-
-
-
 
 
 
